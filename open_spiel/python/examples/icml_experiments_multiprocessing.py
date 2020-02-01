c557edf182128689b1d84e80cecbafa73548e60d
jingya <jxun@sandiego.edu>
2020-02-01 07:21:04 +0000

    pro_update

--- open_spiel/python/examples/icml_experiments_multiprocessing.py
+++ open_spiel/python/examples/icml_experiments_multiprocessing.py
@@ -1,28 +1,42 @@
-from multiprocessing import Process
+import multiprocessing as mp
 import argparse
 import subprocess
 import numpy as np
 from numpy import array
 
-def experiment(args, seed):
-    with open(str(args.out), 'w+') as f:
-        proc = subprocess.Popen(["python", "./neurd_example_tsallis.py", 
+def experiment(args, seed, q):
+        
+    proc = subprocess.Popen(["python", "./neurd_example_tsallis.py", 
                 "--alpha="+str(args.alpha),
                 "--iterations="+str(args.iterations),
                 "--game="+str(args.game),
                 "--random_seed="+str(seed)], stdout=subprocess.PIPE)
 
-        data = []
-        results = str(proc.stdout.read())
 
-        for l in results.split("\\n"):
-            data.append(l.split(" ")[-1])
+    data = []
+    results = str(proc.stdout.read())
+    
+    for l in results.split("\\n"):
+        data.append(l.split(" ")[-1])
 
-        f.write(str(data)+"\n")
+    q.put(data)
+    return data
+
 
 def hello(seed):
     print("hello " + str(seed))
 
+def listener(q, file):
+    '''listens for messages on the q, writes to file. '''
+
+    with open(file, 'w') as f:
+        while 1:
+            m = q.get()
+            if m == 'kill':
+                f.write('killed')
+                break
+            f.write(str(m) + '\n')
+            f.flush()
 
 def main():
 
@@ -36,13 +50,42 @@ def main():
 
     args = parser.parse_args()
 
+    #must use Manager queue here, or will not work
+    manager = mp.Manager()
+    q = manager.Queue()    
+    pool = mp.Pool(mp.cpu_count() + 2)
+
+    #put listener to work first
+    watcher = pool.apply_async(listener, (q,args.out,))
+
+    #fire off workers
+    jobs = []
+    for i in range(args.num_experiments):
+        job = pool.apply_async(experiment, (args, i+1, q,))
+        jobs.append(job)
+
+    # collect results from the workers through the pool result queue
+    for job in jobs: 
+        job.get()
+
+    #now we are done, kill the listener
+    q.put('kill')
+    pool.close()
+    pool.join()
+
     # seeds = array(range(40)) + 1
 
-    for i in range(40):
-        p = Process(target=experiment, args=(args,i,))
-        # p = Process(target=hello, args=(i+1,))
-        p.start()
-        p.join()
+#     processes = []
+#     for i in range(40):
+#         p = Process(target=experiment, args=(args,i,))
+#         # p = Process(target=hello, args=(i+1,))
+#         processes.append(p)
+#         p.start()
+
+#     for p in processes:
+#         p.join()
+
+
 
 
 if __name__ == "__main__":
